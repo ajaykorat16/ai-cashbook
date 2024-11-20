c32f722864d63a3953ccb5f0c1d663e25b4ff8e2
Ajay Korat <ajaykorat16@gmail.com>
2024-11-20 15:30:03 +0530

    Manage speradsheet height

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -38,7 +38,7 @@ const Layout = ({ children }) => {
                             <div id="sidebar" ref={sidebarRef}>
                                 <div className="side_data">
                                     <ul>
-                                        <Link to={"/"} className='header_logo px-2 d-flex justify-content-center'>
+                                        <Link to={"/"} className='header_logo px-2 d-flex justify-content-center mb-3'>
                                             <img src="/images/accoutn_logo_2.png" alt="" />
                                         </Link>
                                         {auth.user.role === 'user' ? (
