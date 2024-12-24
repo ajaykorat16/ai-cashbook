54d54979567b6a0cacf6ff6107e8cade07892e87
Ajay Korat <ajaykorat16@gmail.com>
2024-12-24 16:21:44 +0530

    Manage inter bank

--- client/src/components/SheetComponent.js
+++ client/src/components/SheetComponent.js
@@ -20,13 +20,8 @@ import ClientSelection from './ClientSelection';
 import { groupData } from './data';
 import { useAuth } from '../contexts/AuthContext';
 
-const itrList = ['1.1-FBT Contribution', '1.1-Gross distribution from trusts', '1.1-Gross Income', '1.1-Gross Interest', '1.1-Total Dividends',
-    '1.9-Gov Subsidies', '2.1 - Opening Stock', '2.2-Cost of Sales', '2.3 - Closing Stock', '2.4-40-880 Deduction', '2.4-Contractor fees', '2.4-Superannuation expense',
-    '2.5-Interest paid Australia', '2.5-Interest paid Overseas', '2.5-Rent', '5.1-Depreciation', '2.6-Lease payments Australia', '2.6-Lease payments Overseas', '5.1-Depreciation', '5.2-MV Expenses',
-    '5.3-Repair and Maintenance', '9.1-All Other Expenses', '9.3-Director Fees', '9.2-Non Deductible Expenses']
-
 const SheetComponent = ({ clientId, showSelection }) => {
-    const { getSpreadsheet, updateSpreadsheet, getClientCategory, clientObject, showInterBank, showDateRange, calculateDateRange } = useClient();
+    const { getSpreadsheet, updateSpreadsheet, getClientCategory, clientObject, showInterBank, showDateRange, calculateDateRange, setShowInterBank } = useClient();
     const { toast } = useAuth()
     const spreadsheetRef = useRef(null);
 
@@ -418,9 +413,20 @@ const SheetComponent = ({ clientId, showSelection }) => {
 
     useEffect(() => {
         if (!isLoading) {
-            highlightMatchingRows()
+            if (interBankData.length === 0 && showInterBank) {
+                toast.current?.show({
+                    severity: 'error',
+                    summary: 'Spreadsheet',
+                    detail: "Inter bank is not available.",
+                    life: 3000
+                });
+                setShowInterBank(false)
+            } else {
+                highlightMatchingRows();
+            }
         }
-    }, [showInterBank, isLoading])
+    }, [showInterBank, isLoading]);
+
 
     const getSelectedRowsRange = (range, isSingleAddress) => {
         if (!isSingleAddress) {
